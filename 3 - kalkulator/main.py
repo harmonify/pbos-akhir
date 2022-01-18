fe9bba2cf8c6f855f5c6561c5a8e9747d6046bd6
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2022-01-18 17:55:50 +0700

    add user interface for calculator program

--- 3 - kalkulator/main.py
+++ 3 - kalkulator/main.py
@@ -7,6 +7,15 @@ def main(args=None):
     This calculator implements the Command, Memento, and Builder pattern
     for better extensibility and maintainability.
     """
+    print_demo()
+    print("\n")
+    get_user_command()
+
+
+def print_demo():
+    print("==========================================================")
+    print("Calculator Demo")
+    print("==========================================================")
     # value to start with
     calc = Calculator(10)
     print("Initial Value: ")
@@ -37,14 +46,44 @@ def main(args=None):
     print("\nUndo Operations: ")
     for _ in range(4):
         calc.undo()
-        print("Undo: ", calc)
+        print("Undo.")
+        print("Current value: ", calc)
 
     # Builder pattern
     print("\nExecute multiple commands at once: ")
     print("Add 10, substract 15, multiply by 4, divide by 3: ")
     calc.execute(cmd.AddCommand(10)).execute(cmd.SubstractCommand(15)).execute(
         cmd.MultiplicationCommand(4)).execute(cmd.DividerCommand(3))
-    print(calc)
+    print("Current value: ", calc)
+
+
+def get_user_command():
+    print("==========================================================")
+    print("Calculator Program")
+    print("==========================================================")
+    calc = Calculator(int(input("Enter initial value: ")))
+
+    while True:
+        print("\nCurrent value: ", calc)
+        print("1. Add\n2. Substract\n3. Multiply\n4. Divide\n5. Undo\n6. Print History\n7. Exit")
+        input_cmd = input("Enter command: ")
+        if input_cmd.isdigit() and int(input_cmd) in range(1, 5):
+            input_value = int(input("Enter number: "))
+        print()
+        if input_cmd == "1":
+            calc.execute(cmd.AddCommand(input_value))
+        elif input_cmd == "2":
+            calc.execute(cmd.SubstractCommand(input_value))
+        elif input_cmd == "3":
+            calc.execute(cmd.MultiplicationCommand(input_value))
+        elif input_cmd == "4":
+            calc.execute(cmd.DividerCommand(input_value))
+        elif input_cmd == "5":
+            calc.undo()
+        elif input_cmd == "6":
+            calc.print_history()
+        elif input_cmd == "7":
+            break
 
 
 if __name__ == '__main__':
